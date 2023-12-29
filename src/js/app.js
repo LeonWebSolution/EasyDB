c4b7e9075bacc04b69f3f70a6ad817d621f96a57
LeonWebSolution <leonwebdevv@gmail.com>
2023-12-29 11:11:19 +0300

    pack

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,13 @@
-// import { Fixed } from './components/header-fixed.js';
-// import { Nav } from './components/navbar.js';
-// import { Zoomed } from './components/zoomed-main.js';
-// import { Footer } from './components/dropwodn-footer.js';
-
-// import { SwiperInit } from './components/swiperinit.js';
-// import { Tabs } from './components/tabs.js';
-// import { TextVisible } from './components/text-visible.js';
+import { SwiperInit } from './components/swiperinit.js';
+import { Tabs } from './components/tabs.js';
 import { Accordion } from './components/accordion.js';
-// import { Video } from './components/video.js';
-// import { Table } from './components/table.js';
-
-
 import { Modal } from './components/modal.js';
 
-// const fixedInstance = new Fixed();
-// const NavInstance = new Nav();
-// const zoomedInstance = new Zoomed();
-// const FooterInstance = new Footer();
-// const SwiperInstance = new SwiperInit();
-// const TabsInstance = new Tabs();
-// const TextVisibleInstance = new TextVisible();
+const SwiperInstance = new SwiperInit();
+const TabsInstance = new Tabs();
 const AccordionInstance = new Accordion();
 const ModalInstance = new Modal();
-// const VideoInstance = new Video();
-// const TableInstance = new Table();
+
 
 
 
