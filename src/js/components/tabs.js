2333c092152de84447faed973085063b93f4fbc1
LeonWebSolution <leonwebdevv@gmail.com>
2023-12-29 11:37:19 +0300

    top

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,11 +1,22 @@
 export class Tabs {
-    constructor() {
-        document.addEventListener("DOMContentLoaded", () => {
-            this.initTabs();
-        });
-    }
+  constructor() {
+      document.addEventListener("DOMContentLoaded", () => {
+          this.initTabs();
+      });
+  }
 
-    initTabs() {
-        
-    }
+  initTabs() {
+      var goTopButton = document.querySelector('.footer__go-top');
+
+      // Проверяем, существует ли кнопка
+      if (goTopButton) {
+          goTopButton.addEventListener('click', function() {
+              // Используем плавный скроллинг к верху страницы
+              window.scrollTo({
+                  top: 0,
+                  behavior: 'smooth'
+              });
+          });
+      }
+  }
 }
