a60b46dbddd7d8ef1d4cd12e08328b574b321f02
LeonWebSolution <leonwebdevv@gmail.com>
2023-12-29 11:11:48 +0300

    Updates

---
+++ js/app.js
@@ -0,0 +1,16 @@
+import { SwiperInit } from './components/swiperinit.js';
+import { Tabs } from './components/tabs.js';
+import { Accordion } from './components/accordion.js';
+import { Modal } from './components/modal.js';
+
+const SwiperInstance = new SwiperInit();
+const TabsInstance = new Tabs();
+const AccordionInstance = new Accordion();
+const ModalInstance = new Modal();
+
+
+
+
+// export class Component1 {
+//   // Ваш код здесь
+// }
