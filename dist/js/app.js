ba1878f85db076482dbae1107543f9a67eae4629
LeonWebSolution <leonwebdevv@gmail.com>
2023-12-29 21:35:44 +0300

    policy

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,11 +2,13 @@ import { SwiperInit } from './components/swiperinit.js';
 import { Tabs } from './components/tabs.js';
 import { Accordion } from './components/accordion.js';
 import { Modal } from './components/modal.js';
+import { Mask } from './components/mask.js';
 
 const SwiperInstance = new SwiperInit();
 const TabsInstance = new Tabs();
 const AccordionInstance = new Accordion();
 const ModalInstance = new Modal();
+const MaskInstance = new Mask();
 
 
 
