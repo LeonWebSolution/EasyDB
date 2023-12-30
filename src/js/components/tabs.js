c4af8283af7e3c09a8fcdc5bf1c9af6619ce04ed
LeonWebSolution <leonwebdevv@gmail.com>
2023-12-30 20:01:14 +0300

    project ready

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,22 +1,40 @@
 export class Tabs {
-  constructor() {
+    constructor() {
       document.addEventListener("DOMContentLoaded", () => {
-          this.initTabs();
+        this.initTabs();
       });
-  }
-
-  initTabs() {
-      var goTopButton = document.querySelector('.footer__go-top');
-
-      // Проверяем, существует ли кнопка
-      if (goTopButton) {
-          goTopButton.addEventListener('click', function() {
-              // Используем плавный скроллинг к верху страницы
-              window.scrollTo({
-                  top: 0,
-                  behavior: 'smooth'
-              });
+    }
+  
+    initTabs() {
+      var tabs = document.querySelectorAll('.cases__tab');
+  
+      // Проверяем, существуют ли вкладки
+      if (tabs.length > 0) {
+        tabs.forEach((tab, index) => {
+          tab.addEventListener('click', () => {
+            this.toggleTab(index);
           });
+        });
       }
+    }
+  
+    toggleTab(index) {
+      // Убираем класс active у всех элементов с классом cases__tab
+      var tabs = document.querySelectorAll('.cases__tab');
+      tabs.forEach((tab) => {
+        tab.classList.remove('active');
+      });
+  
+      // Добавляем класс active к выбранному элементу с классом cases__tab
+      tabs[index].classList.add('active');
+  
+      // Убираем класс active у всех элементов с классом cases__block
+      var blocks = document.querySelectorAll('.cases__block');
+      blocks.forEach((block) => {
+        block.classList.remove('active');
+      });
+  
+      // Добавляем класс active к соответствующему элементу с классом cases__block
+      blocks[index].classList.add('active');
+    }
   }
-}
