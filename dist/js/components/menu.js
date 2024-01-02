2371edb8e2f2289b4a1e9df2831ad80475a180cf
LeonWebSolution <leonwebdevv@gmail.com>
2024-01-02 13:16:32 +0300

    ready

--- dist/js/components/menu.js
+++ dist/js/components/menu.js
@@ -1,51 +1,29 @@
 export class Menu {
-    constructor() {
-      // Добавляем слушатель события DOMContentLoaded
-      window.addEventListener("DOMContentLoaded", function() {
-        let prevScrollPos = window.pageYOffset;
-  
-        // Добавляем слушатель события прокрутки страницы
-        window.onscroll = function() {
-          const currentScrollPos = window.pageYOffset;
-  
-          // Проверяем наличие элемента с классом "header"
-          const headerElement = document.querySelector(".header");
-          if (headerElement) {
-            if (prevScrollPos > currentScrollPos) {
-              // Скролл вверх
-              headerElement.classList.add("fixed");
-            } else {
-              // Скролл вниз
-              headerElement.classList.remove("fixed");
-            }
-  
-            // Добавление класса "transform" при скролле на 200px вниз
-            if (currentScrollPos > 200) {
-              headerElement.classList.add("transform");
-            } else {
-              headerElement.classList.remove("transform");
-            }
-  
-            prevScrollPos = currentScrollPos;
-          }
-        };
-      });
-
+  constructor() {
       document.addEventListener('DOMContentLoaded', function () {
-        var hamburger = document.querySelector('.hamburger');
-        var headerMenu = document.querySelector('.header__menu');
-        var hamburgerClose = document.querySelector('.hamburger-close');
-    
-        hamburger.addEventListener('click', function () {
-            headerMenu.classList.add('active');
-        });
-    
-        hamburgerClose.addEventListener('click', function () {
-            headerMenu.classList.remove('active');
-        });
-    });
-    }
-  }
-  
+          var hamburger = document.querySelector('.hamburger');
+          var headerMenu = document.querySelector('.header__menu');
+          var hamburgerClose = document.querySelector('.hamburger-close');
 
-  
+          hamburger.addEventListener('click', function () {
+              headerMenu.classList.add('active');
+          });
+
+          hamburgerClose.addEventListener('click', function () {
+              headerMenu.classList.remove('active');
+          });
+
+          // Добавляем обработчик события для элементов с классом 'nav-link'
+          var navLinks = document.querySelectorAll('.nav__link');
+          navLinks.forEach(function (navLink) {
+              navLink.addEventListener('click', function () {
+                  // Ищем ближайший элемент с классом 'header-menu' и удаляем у него класс 'active'
+                  var closestHeaderMenu = navLink.closest('.header__menu');
+                  if (closestHeaderMenu) {
+                      closestHeaderMenu.classList.remove('active');
+                  }
+              });
+          });
+      });
+  }
+}
